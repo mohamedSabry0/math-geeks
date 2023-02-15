1735de78d1dec84c1137ae35c3481bb6e5e23fe1
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-02-15 18:50:47 +0200

    add buttons for the numbers

--- src/components/buttonsArea.js
+++ src/components/buttonsArea.js
@@ -1,12 +1,37 @@
+import PropTypes from 'prop-types';
+
 function SpecialOperations() {
   return (
     <div className="special-container" />
   );
 }
 
+function Button(props) {
+  const { s, val } = props;
+  return (
+    <button type="button" className={`w-${s}`}>{val}</button>
+  );
+}
+
+Button.propTypes = { s: PropTypes.number, val: PropTypes.number };
+
+Button.defaultProps = { s: 3, val: '.' };
+
 function Numbers() {
   return (
-    <div className="numbers-container" />
+    <div className="numbers-container">
+      <Button val={1} />
+      <Button val={2} />
+      <Button val={3} />
+      <Button val={4} />
+      <Button val={5} />
+      <Button val={6} />
+      <Button val={7} />
+      <Button val={8} />
+      <Button val={9} />
+      <Button s={6} val={0} />
+      <Button />
+    </div>
   );
 }
 
